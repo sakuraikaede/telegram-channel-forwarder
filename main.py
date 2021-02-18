caff34b3dbdb77ddf87fc2030607b92866f6c7a2
sakuraikaede <sakurai.kaede@outlook.com>
2021-02-18 12:23:33 +0800

    Change license and update codes

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "向我发送图片/网址就可以啦w")
+
+@bot.message_handler(content_types=["text", "sticker", "photo", "audio"], func=lambda message: True)
+def forward_all(message):
+	bot.forward_message("-100XXXXXXXXXX", message.chat.id, message.message_id)
+bot.polling(none_stop=True)
